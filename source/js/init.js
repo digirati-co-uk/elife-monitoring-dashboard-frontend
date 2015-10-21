e83660e7f99ac4b2a7fe00181fe5f414622bd3e3
Saad Sher <saad.sher@digirati.com>
2015-10-21 16:41:39 +0100

    navigation changed to be responsive; changed article lists to be semantically correct as tables; fixed hover state on some buttons; deleting unused things in PL

--- source/js/init.js
+++ source/js/init.js
@@ -24,97 +24,134 @@
 	});
 
 	//Global variables
-	var articlesQueue = [];
+	var articlesQueue = {};
 	var articleId, articleVer, queueDress;
 
-	//Publish Checkbox
+	//Article Checkbox
 	$('.article-snapshot-list.action input:checkbox').click(function(e) {
-
-		$("#publish-action").hide();
-		$("#publish-all-action").show();
-
-		if ($("#articles-queue").length == 0) {
-			$("#article-queue").detach();
-			$("#publish-modal .modal-body").append("<ol id='articles-queue'></ol>");
-		}
 
 		if ($(".article-snapshot-list.action input:checkbox:checked").length > 0) {
 			$(".btn#publish").attr('disabled', true);
-			$("#publish-all").show();
 		} else {
 			$(".btn#publish").attr('disabled', false);
+		}
+
+		if ($(".article-snapshot-list.action input:checkbox:checked").length > 1) {
+			$("#publish-all").show();
+		} else {
 			$("#publish-all").hide();
 		}
-
-		articleId = $(this).parents("li").find(".article-id").html();
-		articleVer = $(this).parents("li").find(".article-version").html();
-		queueDress = articleId + "-v" + articleVer;
-
-		if (this.checked) {
-			articlesQueue.push(queueDress);
-			$("#articles-queue").append("<li>" + queueDress + "</li>");
-		} else {
-		 	articlesQueue.pop(queueDress);
-			$("#articles-queue li:contains('" + queueDress + "')").remove();
-		}
 	});
-	//Publish
+	//Article Publish Click
 	$(".btn#publish").click(function(e) {
 
+		$("#articles-queue").empty();
 		$("#publish-all-action").hide();
 		$("#publish-action").show();
-
-		if ($("#article-queue").length == 0) {
-			$("#articles-queue").detach();
-			$("#publish-modal .modal-body").append("<ol id='article-queue'></ol>");
-		}
-
-		articleId = $(this).parents("li").find(".article-id").html();
-		articleVer = $(this).parents("li").find(".article-version").html();
+		
+		articleId = $(this).parents("tr").find(".article-id").html();
+		articleVer = $(this).parents("tr").find(".article-version").html();
 		queueDress = articleId + "-v" + articleVer;
 
-		$("#article-queue li").remove();
-		$("#article-queue").append("<li>" + queueDress + "</li>");
-	});
-	//Publish Cancel
-	$(".btn#publish-cancel").click(function(e) {
+		articlesQueue = {};
+		articlesQueue[queueDress] = "";
+		// articlesQueue[JSON.stringify(queueDress)] = "";
 
-		$("#article-queue, #articles-queue").detach();
-		location.reload(true);
-		console.log("page reloaded");
+		$("#articles-queue").append("<li>" + queueDress + "</li>");
+
+		// console.log(articlesQueue);
 	});
-	//Publish Action
+	//Article Publish All Click
+	$(".btn#publish-all").click(function(e) {
+
+		$("#articles-queue").empty();
+		$("#publish-all-action").show();
+		$("#publish-action").hide();
+
+		articlesQueue = {};
+
+		$(".article-snapshot-list.action input:checkbox:checked").each(function() {
+			
+			articleId = $(this).parents("tr").find(".article-id").html();
+			articleVer = $(this).parents("tr").find(".article-version").html();
+			queueDress = articleId + "-v" + articleVer;
+
+
+			articlesQueue[queueDress] = "";
+
+			$("#articles-queue").append("<li>" + queueDress + "</li>");
+		});
+
+		// console.log(articlesQueue);
+	});
+	//Publish/Publish All Action
 	$("#publish-action, #publish-all-action").click(function(e) {
 
-		// Process
-		// $.ajax({
-		//   type: 'POST',
-		//   url: '../../data/queue.json',
-		//   success: function(data, textStatus) {
-		//     $.each(data, function(key, value) {
-		// 		if (value === "error") {
-		// 			console.log("error");
-		// 		} else if (value === "published") {
-		// 			console.log("published");
-		// 		} else if (value === "ready to publish") {
-		// 			console.log("ready to publish");
-		// 		} else if (value === null) {
-		// 			console.log("null");
-		// 		} else {
-		// 			console.log("not handled");
-		// 		}
-		// 	});
-		//   },
-		//   error: function(xhr, textStatus, errorThrown) {
-		//     var err = textStatus + ", " + errorThrown;
-		// 	console.log( "Request Failed: " + err );
-		//   }
-		// });
+		e.stopPropagation();
+		$(this).attr('disabled', true);
+
+		//Publish Queue
+		sendQueue();
 
 		//Response Loop
 		getResponse();
-	});
 
+		//Publish Cancel
+		$("#publish-modal .modal-dialog").click(function(e) {
+			e.stopPropagation();
+		});
+		$("#publish-modal button.close, .btn#publish-cancel").click(function(e) {
+
+			e.stopPropagation();
+			location.reload(true);
+		});
+		$(document).keyup(function(e) {
+			if (e.keyCode == 27) {
+				location.reload(true);
+			}
+		});
+	});
+	//Poll
+	function sendQueue() {
+		$.ajax({
+			type: 'POST',
+			// url: '../../data/queue.json',
+			url: '../../data/queue.json',
+			data: articlesQueue,
+		    error: function(xhr, textStatus, errorThrown) {
+	      		var err = textStatus + ", " + errorThrown;
+				console.log( "Request Failed: " + err );
+		  	}
+		}).done(function(data, textStatus) {
+
+			// console.log(articlesQueue);
+
+		    $.each(data, function(key, value) {
+
+		    	if (articlesQueue.hasOwnProperty(key) && articlesQueue[key] === "queued") {
+
+					var keyCheck = "#articles-queue li:contains('" + key + "')";
+
+					// build up the object array with the values from the server
+
+					// set the publish / publish all spinner in progress
+					$("#publish-action, #publish-all-action").append("<div class='throbber-loader'>Loading…</div>");
+
+					// if (value.result === "error") {
+					// 	console.log(value[0].result + ": " + value[1].message);
+					// 	$(keyCheck).append("<span class='glyphicon glyphicon-remove'></span>");
+					// } else if (value.result === "queued") {
+					// 	console.log(value[0].result);
+					// 	$("#publish-action, #publish-all-action").empty().append("<div class='throbber-loader'>Loading…</div>");
+					// } else {
+					// 	console.log(value[0]);
+					// }
+				} else {
+					// show errors to the user next to articles
+				}
+			});
+		});
+	}
 	//Responses
 	function getResponse() {
 		$.ajax({
@@ -122,37 +159,46 @@
 			url: '../../data/response.json',
 			success: function(data, textStatus) {
 
+				// poll for articles that have not yet been published recursively
+
 			    $.each(data, function(key, value) {
 
-					if (jQuery.inArray(key, articlesQueue) !== -1 || $("#article-queue")) {
+			    	// check if key exists and its value is not 'published'
+					if (articlesQueue.hasOwnProperty(key) && articlesQueue[key] !== "published") {
 
-						var keyCheck = "#article-queue li:contains('" + key + "'), #articles-queue li:contains('" + key + "')";
+						var keyCheck = "#articles-queue li:contains('" + key + "')";
+
+						$(keyCheck).children().remove();
 
 						if (value === "error") {
-							$(keyCheck).append("<span> error</span>");
+							$(keyCheck).append("<span class='glyphicon glyphicon-remove' data-toggle='tooltip' data-placement='top' title='Error'></span>");
 						} else if (value === "published") {
-							$(keyCheck).append("<span> published</span>");
-						} else if (value === "ready to publish") {
-							$(keyCheck).append("<span> ready to publish</span>");
-						} else if (value === null) {
-							$(keyCheck).append("<span> null</span>");
+							$(keyCheck).append("<span class='glyphicon glyphicon-ok' data-toggle='tooltip' data-placement='top' title='Published'></span>");
+						} 
+						// else if (value === "ready to publish") {
+						// 	$(keyCheck).append("");
+						// } 
+						else if (value === null) {
+							$(keyCheck).append("<span class='glyphicon glyphicon-warning-sign' data-toggle='tooltip' data-placement='top' title='Something went wrong'></span>");
 						} else {
 							console.log(value);
 						}
 					}
 				});
 
+				//Initialise Tooltip
+				$('[data-toggle="tooltip"]').tooltip();
+
 				setTimeout(function() {
 					getResponse();
 				}, 10000);
 			},
 			error: function(xhr, textStatus, errorThrown) {
-
 			    var err = textStatus + ", " + errorThrown;
 				console.log( "Request Failed: " + err );
 			}
 		});
-	};
+	}
 
 	//Filter Box
 	$('#filter .dropdown-menu').on({
