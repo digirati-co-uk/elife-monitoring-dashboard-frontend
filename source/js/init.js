9536bdfbef34e22948dace4a2ce73d6816202181
Saad Sher <saad.sher@digirati.com>
2015-10-23 17:28:23 +0100

    modal behaviour, improvement on JS for the publishing process - not yet complete / tested; added placeholder to datepicker

--- source/js/init.js
+++ source/js/init.js
@@ -23,6 +23,13 @@
 		$('.header .search-form').toggleClass('active');
 	});
 
+	//Filter Box
+	$('#filter .dropdown-menu').on({
+		"click":function(e) {
+	      e.stopPropagation();
+	    }
+	});
+
 	//Datepicker
 	$('#datetimepicker-start').datetimepicker({
 		format: 'D-MM-YY'
@@ -39,69 +46,82 @@
     });
 
 	//Global variables
-	var articlesQueue = {};
-	var articleId, articleVer, queueDress;
+	var articlesQueue = {}; //JS Object that holds the article-ids to be queued/published
+	var articleId, articleVer, queueDress; //Variables to prepare the article-id(s) for use
 
 	//Article Checkbox
 	$('.article-snapshot-list.action input:checkbox').click(function(e) {
 
+		//Interaction in the actionable items column of UIR table
 		if ($(".article-snapshot-list.action input:checkbox:checked").length > 0) {
 			$(".btn#publish").attr('disabled', true);
+			if ($(".article-snapshot-list.action input:checkbox:checked").length > 1) {
+				$("#publish-all").show();
+			} else {
+				$("#publish-all").hide();
+			}
 		} else {
 			$(".btn#publish").attr('disabled', false);
 		}
-
-		if ($(".article-snapshot-list.action input:checkbox:checked").length > 1) {
-			$("#publish-all").show();
-		} else {
-			$("#publish-all").hide();
-		}
 	});
+
 	//Article Publish Click
 	$(".btn#publish").click(function(e) {
 
+		//Empty list & swap text to 'publish'
 		$("#articles-queue").empty();
-		$("#publish-all-action").hide();
-		$("#publish-action").show();
-		
+		// $("#publish-all-action").hide();
+		$("#publish-action").empty().text('Publish');
+
+		//Prepare the article-ids for insertion in JS object
 		articleId = $(this).parents("tr").find(".article-id").html();
 		articleVer = $(this).parents("tr").find(".article-version").html();
 		queueDress = articleId + "-v" + articleVer;
 
+		//Empty & populate the JS Object
 		articlesQueue = {};
 		articlesQueue[queueDress] = "";
 		// articlesQueue[JSON.stringify(queueDress)] = "";
 
+		//Feedback to the user
 		$("#articles-queue").append("<li>" + queueDress + "</li>");
 
 		// console.log(articlesQueue);
 	});
+
 	//Article Publish All Click
 	$(".btn#publish-all").click(function(e) {
 
+		//Empty list & swap text to 'Publish all'
 		$("#articles-queue").empty();
-		$("#publish-all-action").show();
-		$("#publish-action").hide();
+		// $("#publish-all-action").show();
+		$("#publish-action").empty().text('Publish all');
 
+		//Empty the JS Object
 		articlesQueue = {};
 
+		//Iterate through selected article(s)
 		$(".article-snapshot-list.action input:checkbox:checked").each(function() {
 			
+			//Prepare the article-ids for insertion in JS object & queue list
 			articleId = $(this).parents("tr").find(".article-id").html();
 			articleVer = $(this).parents("tr").find(".article-version").html();
 			queueDress = articleId + "-v" + articleVer;
 
-
+			//Add to the JS Object
 			articlesQueue[queueDress] = "";
 
+			//Append to the queue list
 			$("#articles-queue").append("<li>" + queueDress + "</li>");
 		});
 
 		// console.log(articlesQueue);
 	});
-	//Publish/Publish All Action
-	$("#publish-action, #publish-all-action").click(function(e) {
 
+	//Publish (all) Action
+	$("#publish-action").click(function(e) {
+
+		//Disable Publish (all) button to stop sending multiple requests
 		e.stopPropagation();
 		$(this).attr('disabled', true);
 
@@ -111,26 +131,23 @@
 		//Response Loop
 		getResponse();
 
-		//Publish Cancel
-		$("#publish-modal .modal-dialog").click(function(e) {
-			e.stopPropagation();
-		});
+		//Click Cancel / Close icon to close modal & force reload
 		$("#publish-modal button.close, .btn#publish-cancel").click(function(e) {
-
 			e.stopPropagation();
 			location.reload(true);
 		});
+		//Or press ESC & force a reload
 		$(document).keyup(function(e) {
 			if (e.keyCode == 27) {
 				location.reload(true);
 			}
 		});
 	});
+
 	//Poll
 	function sendQueue() {
 		$.ajax({
 			type: 'POST',
-			// url: '../../data/queue.json',
 			url: '../../data/queue.json',
 			data: articlesQueue,
 		    error: function(xhr, textStatus, errorThrown) {
@@ -141,84 +158,86 @@
 
 			// console.log(articlesQueue);
 
+			//Loop through all the key, value pairs in returned JS Object from server
 		    $.each(data, function(key, value) {
 
-		    	if (articlesQueue.hasOwnProperty(key) && articlesQueue[key] === "queued") {
+		    	//Check if key exists in JS Object and the same key from server's response has the value 'queued'
+		    	if (articlesQueue.hasOwnProperty(key) && key.result === "queued") {
 
-					var keyCheck = "#articles-queue li:contains('" + key + "')";
+					//If not showing the spinner
+					if ($("#publish-action *:not(div)")) {
+						//Show the publish (all) spinner in progress
+						$("#publish-action").empty().append("<div class='throbber-loader'>Loading…</div>");
+					}
 
-					// build up the object array with the values from the server
+					//Edit the JS Object with the value from server's response
+					articlesQueue[key] = value;
 
-					// set the publish / publish all spinner in progress
-					$("#publish-action, #publish-all-action").append("<div class='throbber-loader'>Loading…</div>");
+				} else if (articlesQueue.hasOwnProperty(key) && key.result === "error") {
 
-					// if (value.result === "error") {
-					// 	console.log(value[0].result + ": " + value[1].message);
-					// 	$(keyCheck).append("<span class='glyphicon glyphicon-remove'></span>");
-					// } else if (value.result === "queued") {
-					// 	console.log(value[0].result);
-					// 	$("#publish-action, #publish-all-action").empty().append("<div class='throbber-loader'>Loading…</div>");
-					// } else {
-					// 	console.log(value[0]);
-					// }
+		    		var keyCheck = "#articles-queue li:contains('" + key + "')";
+
+					//Show error(s) to the user next to article(s) & Remove article-id(s) from JS Object
+					$(keyCheck).append("<span class='glyphicon glyphicon-remove' data-toggle='tooltip' data-placement='top' title='" + key.message + "'></span>");
+					delete articlesQueue[key];
+
 				} else {
-					// show errors to the user next to articles
+					console.log(key + ": Invalid article");
 				}
 			});
 		});
 	}
+
 	//Responses
 	function getResponse() {
 		$.ajax({
 			type: 'GET',
 			url: '../../data/response.json',
-			success: function(data, textStatus) {
-
-				// poll for articles that have not yet been published recursively
-
-			    $.each(data, function(key, value) {
-
-			    	// check if key exists and its value is not 'published'
-					if (articlesQueue.hasOwnProperty(key) && articlesQueue[key] !== "published") {
-
-						var keyCheck = "#articles-queue li:contains('" + key + "')";
-
-						$(keyCheck).children().remove();
-
-						if (value === "error") {
-							$(keyCheck).append("<span class='glyphicon glyphicon-remove' data-toggle='tooltip' data-placement='top' title='Error'></span>");
-						} else if (value === "published") {
-							$(keyCheck).append("<span class='glyphicon glyphicon-ok' data-toggle='tooltip' data-placement='top' title='Published'></span>");
-						} 
-						// else if (value === "ready to publish") {
-						// 	$(keyCheck).append("");
-						// } 
-						else if (value === null) {
-							$(keyCheck).append("<span class='glyphicon glyphicon-warning-sign' data-toggle='tooltip' data-placement='top' title='Something went wrong'></span>");
-						} else {
-							console.log(value);
-						}
-					}
-				});
-
-				//Initialise Tooltip
-				$('[data-toggle="tooltip"]').tooltip();
-
-				setTimeout(function() {
-					getResponse();
-				}, 10000);
-			},
+			data: articlesQueue,
 			error: function(xhr, textStatus, errorThrown) {
-			    var err = textStatus + ", " + errorThrown;
+	      		var err = textStatus + ", " + errorThrown;
 				console.log( "Request Failed: " + err );
-			}
+		  	}
+		}).done(function(data, textStatus) {
+
+			//Loop through all the key, value pairs in returned JS Object from server
+		    $.each(data, function(key, value) {
+
+		    	var keyCheck = "#articles-queue li:contains('" + key + "')";
+
+		    	//Check if key exists in JS Object and the same key from server's response has the value 'published'
+				if (articlesQueue.hasOwnProperty(key) && value === "published") {
+
+					//Show published status to the user next to article(s) & Remove article-id(s) from JS Object
+					$(keyCheck).append("<span class='glyphicon glyphicon-ok' data-toggle='tooltip' data-placement='top' title='" + value + "'></span>");
+					delete articlesQueue[key];
+				} else if (articlesQueue.hasOwnProperty(key) && value === "error") {
+
+					//Show error status to the user next to article(s) & Remove article-id(s) from JS Object
+					$(keyCheck).append("<span class='glyphicon glyphicon-remove' data-toggle='tooltip' data-placement='top' title='" + value + "'></span>");
+					delete articlesQueue[key];
+				} else if (articlesQueue.hasOwnProperty(key) && value === null) {
+
+					//Show error status to the user next to article(s) & Remove article-id(s) from JS Object
+					$(keyCheck).append("<span class='glyphicon glyphicon-remove' data-toggle='tooltip' data-placement='top' title='" + value + "'></span>");
+					delete articlesQueue[key];
+
+					console.log(key + ": No status message");
+				} else {
+					console.log(key + ": Invalid article");
+				}
+			});
+
+			//Initialise Tooltips
+			$('[data-toggle="tooltip"]').tooltip();
+
+			//Loop this function every 10 seconds
+			setTimeout(function() {
+				getResponse();
+			}, 10000);
+
+			// TODO
+			// Break loop once all articles in queue have been handled and feed back to the user
 		});
 	}
-
-	//Filter Box
-	$('#filter .dropdown-menu').on({
-		"click":function(e) {
-	      e.stopPropagation();
-	    }
-	});
 })(this);
