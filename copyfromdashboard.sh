5ff9a1cc0f64c5bd103b2be7971434b913b4bca9
Hannah <xhan104@gmail.com>
2016-05-26 15:18:23 +0100

    move folders

---
+++ copyfromdashboard.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#set -x
+
+# ./copyfromdashboard.sh DASHBOARD
+# ./copyfromdashboard.sh ~/Projects/eLife/elife-dashboard/
+
+#DASHBOARD="~/Projects/eLife/elife-dashboard/"
+DASHBOARD=$1
+
+cp -rp "$DASHBOARD/_src/libs" _src/libs
+cp -rp "$DASHBOARD/_src/scss" _src/scss
+cp -rp "$DASHBOARD/dashboard/static/js" _src/js
+cp -rp "$DASHBOARD/dashboard/static/images" source/images
+cp -rp "$DASHBOARD/dashboard/static/fonts" source/fonts
